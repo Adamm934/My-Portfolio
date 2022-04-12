1783baf87b2cdb11a792ef0bd04929c4f99e476b
Adam Malinovski <adammm934@gmail.com>
2022-04-12 20:49:26 +0100

    starting hero section

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
-const hamburgerContainer = document.querySelector('#header__main-nav');
+const hamburgerContainer = document.querySelector("#header__main-nav");
 
 const hamburger = document.querySelector(".header__main-nav--hamburger");
-const links = document.querySelector('.header__main-nav--links li')
+const links = document.querySelectorAll(".header__main-nav--links li");
 
-hamburger.addEventListener('click', ()=> {
-    hamburgerContainer.classList.toggle('clicked')
-})
+// event listener corresponding to css
+hamburger.addEventListener("click", () => {
+  hamburgerContainer.classList.toggle("clicked");
+  links.forEach(function (link) {
+          link.classList.toggle('fade');
+      });
+});
